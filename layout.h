653ad6d7b4501b7485fe2eadb6ff75d6dc6b4aac
nesth <nesth42@gmail.com>
2022-11-09 11:49:50 -0500

    Update layout.h

--- layout.h
+++ layout.h
@@ -12,7 +12,7 @@
 // Default 3x6_3 split layout
 #define _BASE \
     KC_TAB,     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSPC,  \
-    QK_LCTL,    KC_A,   KC_S,   KC_D,   KC_F,   KC_G,       KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT, \
+    KC_LCTL,    KC_A,   KC_S,   KC_D,   KC_F,   KC_G,       KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT, \
     KC_LSPO,    KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,       KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSPC, \
                                 KC_LGESC,LOWER, KC_SPC,     KC_ENT, RAISE,  KC_RACL
 
